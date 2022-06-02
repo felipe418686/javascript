5ca34d344cda00cfbff50ede3bff06c75b17570a
felipe418686 <felipe418686@gmail.com>
2022-06-02 18:40:13 -0300

    creo un calculador de iva JS

--- js/main.js
+++ js/main.js
@@ -1,15 +1,54 @@
-let ingreso = parseInt (prompt("ingrese un numero a multiplicar"));
+/*let ingreso = parseInt (prompt("ingrese un numero a multiplicar"));
 let salida = parseInt (prompt("ingrese cuantas veces queres multiplicar"));
-
+//ingreso un for para que el usario ingrese 2 numeros a multiplicar 
 for (let i = 1; i <= salida; i= i + 1){
     let resultado = ingreso * i;
+    //agrego un continue para saltar la linea 9 y seguir hasta la indicada, es un especie de break
     if (i==9) {
         continue
     }
+    //coloco el console log para que el usario vea el resultado
     console.log(ingreso + " X " + i + " = " + resultado);
 }
 let entrada = prompt("ingresa la lista del super \n Para salir ingresa ESC");
 while (entrada != "ESC") {
     console.log("la lista es: " + entrada );
     entrada = prompt("ingresa la lista del super \n Para salir ingresa ESC");
+}*/
+//Ejercicio 2
+let precioProducto = parseFloat(prompt("ingrese el precio del producto"));
+//Aqui el usuario va a poner el precio del producto a calcular incluyendo el iva
+while(true){
+    if(!isNaN(precioProducto) && precioProducto != null && precioProducto != ""){
+    break;
+    }else{
+        //esto se ejecuta si el usuario no ingresa un numero
+        alert('no es numero');
+        continue;
+    }
 }
+    precioProductoM = precioProducto.toFixed(2);
+    alert("el precio del producto es: " + precioProductoM);
+    console.log("el precio del producto es: " + precioProductoM);
+
+    //Sumar el IVA al producto
+    function sumarIva(precioProducto){
+    return (precioProducto * 0.21)
+    }
+
+    let iva = sumarIva(precioProducto);
+    ivaM = iva.toFixed(2);
+
+    alert("el IVA es: " + ivaM);
+    console.log("el IVA es: " + ivaM);
+
+    //Sumar el precio del producto con el iva
+    function sumarPrecio(precioProducto, iva){
+    return (precioProducto * 0.21 + precioProducto)
+    }
+    let precioTotal = sumarPrecio(precioProducto, iva);
+    precioTotalM = precioTotal.toFixed(2);
+
+    alert("el precio total es: " + precioTotalM);
+    console.log("el precio total es: " + precioTotalM);
+
